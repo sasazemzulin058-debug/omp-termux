76b3487254f90af40cc831fa83dac5e91e0761fd
sasazemzulin058-debug <sasazemzulin058@gmail.com>
2026-08-19 20:16:40 +0500

    fix(android): avoid Bun JSON import crash in native loader

--- packages/natives/native/loader-state.js
+++ packages/natives/native/loader-state.js
@@ -4,7 +4,8 @@ import { createRequire } from "node:module";
 import * as os from "node:os";
 import * as path from "node:path";
 import * as zlib from "node:zlib";
-import packageJson from "../package.json" with { type: "json" };
+const require = createRequire(import.meta.url);
+const packageJson = require("../package.json");
 import { embeddedAddon } from "./embedded-addon.js";
 
 /**
