81ad70746a09dbc17de6ebf637d99b9090fa6b3d
sasazemzulin058-debug <sasazemzulin058@gmail.com>
2026-08-26 22:04:45 +0500

    fix(android): include desktop-adapter.js in termux JS bundle (fixes v18.0.6)

--- android/scripts/build-termux-js.ts
+++ android/scripts/build-termux-js.ts
@@ -86,7 +86,7 @@ async function main(): Promise<void> {
 		const nativesPkg = path.join(repoRoot, "packages/natives/package.json");
 		const nativesNative = path.join(repoRoot, "packages/natives/native");
 		await Bun.$`cp ${nativesPkg} ${bundleDir}/node_modules/@oh-my-pi/pi-natives/`.quiet();
-		for (const name of ["index.js", "loader-state.js", "embedded-addon.js", "clipboard.js", "desktop.js"]) {
+		for (const name of ["index.js", "loader-state.js", "embedded-addon.js", "clipboard.js", "desktop.js", "desktop-adapter.js"]) {
 			await Bun.$`cp ${path.join(nativesNative, name)} ${bundleDir}/node_modules/@oh-my-pi/pi-natives/native/`.quiet();
 		}
 
